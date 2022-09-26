127c455a163f0ef1ef24d8cdbe3036df08d5752f
letChristianSosa <chrissm28@outlook.com>
2022-09-25 21:14:49 -0500

    Fin

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,64 @@
+import { useState } from "react";
+import useLetras from "../hooks/useLetras";
+
+const Formulario = () => {
+  const { setAlerta, busquedaLetra } = useLetras();
+
+  const [busqueda, setBusqueda] = useState({
+    artista: "",
+    cancion: "",
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (Object.values(busqueda).includes("")) {
+      setAlerta("Coloca nombre de artista y cancion");
+      return;
+    }
+    busquedaLetra(busqueda);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <legend>Busca por Artistas y Cancion</legend>
+      <div className="form-grid">
+        <div>
+          <label>Artista</label>
+          <input
+            type="text"
+            name="artista"
+            placeholder="Nombre del artista"
+            value={busqueda.artista}
+            onChange={(e) =>
+              setBusqueda({
+                ...busqueda,
+                [e.target.name]: e.target.value,
+              })
+            }
+          />
+        </div>
+
+        <div>
+          <label>Cancion</label>
+          <input
+            type="text"
+            name="cancion"
+            placeholder="Nombre de la cancion"
+            value={busqueda.cancion}
+            onChange={(e) =>
+              setBusqueda({
+                ...busqueda,
+                [e.target.name]: e.target.value,
+              })
+            }
+          />
+        </div>
+
+        <input type="submit" value="Buscar" />
+      </div>
+    </form>
+  );
+};
+
+export default Formulario;
---
+++ src/components/Letra.jsx
@@ -0,0 +1,9 @@
+import useLetras from "../hooks/useLetras";
+
+const Letra = () => {
+  const { letra, cargando } = useLetras();
+
+  return cargando ? "Cargando..." : <div className="letra">{letra}</div>;
+};
+
+export default Letra;
